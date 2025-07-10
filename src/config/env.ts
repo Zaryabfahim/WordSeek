7df755015199af21041469d5fe98fdaa683e25ba
Zaryabfahim <darerider537freefire@gmail.com>
2025-07-10 23:15:45 +0530

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const env = z
   .object({
-    BOT_TOKEN: z.string().min(1, { message: "BOT_TOKEN is required" }),
-    DATABASE_URI: z.string().min(1, { message: "DATABASE_URI is required" }),
+    BOT_TOKEN: z.string().min(1, { message: "7949443460:AAFI1Uiiye9JtG_EJzLVSvl6r8d4fK9_JXs" }),
+    DATABASE_URI: z.string().min(1, { message: "mongodb+srv://Zaryab:Zaryab123@cluster0.mge1ngz.mongodb.net/" }),
     NODE_ENV: z.enum(["development", "production"]).default("development"),
     ADMIN_USERS: z
       .string()
