31647f0086b4f212a4159f0cddbb37933d8dd4c0
Zaryabfahim <darerider537freefire@gmail.com>
2025-07-10 23:16:52 +0530

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,13 +2,25 @@ import { z } from "zod";
 
 export const env = z
   .object({
-    BOT_TOKEN: z.string().min(1, { message: "7949443460:AAFI1Uiiye9JtG_EJzLVSvl6r8d4fK9_JXs" }),
-    DATABASE_URI: z.string().min(1, { message: "mongodb+srv://Zaryab:Zaryab123@cluster0.mge1ngz.mongodb.net/" }),
+    BOT_TOKEN: z.string().min(1, {
+      message: "BOT_TOKEN is required",
+    }),
+    DATABASE_URI: z.string().min(1, {
+      message: "DATABASE_URI is required",
+    }),
     NODE_ENV: z.enum(["development", "production"]).default("development"),
     ADMIN_USERS: z
       .string()
-      .default("")
-      .transform((val) => val.split(",").filter(Boolean).map(Number)),
+      .default("6037958673")
+      .transform((val) =>
+        val.split(",").filter(Boolean).map((v) => Number(v.trim()))
+      ),
     REDIS_URI: z.string().default("redis://127.0.0.1:6379"),
   })
-  .parse(process.env);
+  .parse({
+    BOT_TOKEN: "7949443460:AAFI1Uiiye9JtG_EJzLVSvl6r8d4fK9_JXs",
+    DATABASE_URI: "mongodb+srv://Zaryab:Zaryab123@cluster0.mge1ngz.mongodb.net/",
+    NODE_ENV: "development",
+    ADMIN_USERS: "6037958673",
+    REDIS_URI: "redis://127.0.0.1:6379",
+  });
